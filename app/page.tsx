85f35d01c2ba6c6431bf129b7e3bc669cb46a63b
Chase Peterson <cep.piano@gmail.com>
2024-03-01 16:46:08 -0800

    Fixed Navbar bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/NavBar";
+import Navbar from "@/components/Navbar";
 import EventList from "@/components/EventList";
 import Contributors from "@/components/Contributors";
 import Footer from "@/components/Footer";
