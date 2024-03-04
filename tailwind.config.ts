b15785283adedf6cf81f13bbec39af2917cb16bb
Chase Peterson <cep.piano@gmail.com>
2024-03-03 19:29:32 -0800

    Added Inter fontFamily in Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,6 +80,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        inter: ["var(--font-inter)"]
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
