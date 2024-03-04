c274f132eec25b964f54124f32f41389e6161c6e
Chase Peterson <cep.piano@gmail.com>
2024-03-03 19:29:41 -0800

    Finished basic navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,10 +9,18 @@ import Link from "next/link";
 export default function Navbar() {
 	return (
 		<>
-			<div className="fixed h-20 text-vgdc-light-green w-full backdrop-blur-lg bg-background-black/30">
-				<Link href="./" className="absolute top-2 left-2 cursor-wait" >
-					<Image src="/logos/VGDC-logo.png" alt="VGDC Logo" width={380} height={780} className="cursor-pointer w-36" />
-				</Link>
+			<div className="fixed h-20 text-light-grey w-full font-light backdrop-blur-lg bg-background-black/30">
+				<div className="relative w-full px-8 py-4">
+					<Link href="./" className="absolute w-fit block top-4" >
+						<Image src="/logos/VGDC-logo-no-subtitle.png" alt="VGDC Logo" width={300} height={780} className="cursor-pointer w-32" />
+					</Link>
+					<div className="relative align-middle space-x-20 mx-auto w-fit text-2xl top-3">
+						<Link href="./">Home</Link>
+						<Link href="./officers">Team</Link>
+						<Link href="./events">Events</Link>
+						<Link href="./news">News</Link>
+					</div>
+				</div>
 			</div>
 			<div className="pointer-events-none h-20 " />
 		</>
