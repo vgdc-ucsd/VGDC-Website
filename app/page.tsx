f5328ae978996d696876dcc2c5da144fd292c5a4
Chase Peterson <cep.piano@gmail.com>
2024-03-06 13:28:19 -0800

    Implemented Will's changes

--- app/page.tsx
+++ app/page.tsx
@@ -26,13 +26,13 @@ function Hero() {
       {/* The full container */}
       <div className="relative h-72 w-full sm:h-96 md:h-[28rem] xl:h-[44rem]">
         <div className="absolute left-0 top-0 w-full">
-          <div className="relative mx-auto w-80 sm:w-[36rem] md:w-[44rem] xl:w-[68rem]">
+          <div className="relative mx-auto w-80 sm:w-[28rem] md:w-[32rem] xl:w-[38rem]">
             <Image
               src="/icons/controller.svg"
               alt="Controller icon"
               width={128}
               height={128}
-              className="absolute left-4 top-16 w-12 sm:w-20 md:top-24 md:w-24 xl:w-40"
+              className="absolute left-4 top-16 w-12 sm:w-20 md:top-24 xl:top-36 xl:w-24"
             />
 
             <Image
@@ -40,7 +40,7 @@ function Hero() {
               alt="Controller icon"
               width={128}
               height={128}
-              className="absolute right-4 top-16 w-12 sm:w-20 md:top-24 md:w-24 xl:w-40"
+              className="absolute right-4 top-16 w-12 sm:w-20 md:top-24 xl:top-36 xl:w-24"
             />
           </div>
         </div>
@@ -51,7 +51,7 @@ function Hero() {
             className="mt-24 inline-block
 							bg-gradient-to-r from-vgdc-light-blue to-vgdc-light-green bg-clip-text 
 							text-center font-inter text-3xl font-extrabold text-transparent
-							sm:mt-32 sm:text-5xl md:mt-40 md:text-6xl xl:mt-56 xl:text-8xl"
+							sm:mt-32 sm:text-4xl md:mt-40 xl:mt-56 xl:text-5xl"
           >
             <b>
               Video Game
@@ -60,16 +60,16 @@ function Hero() {
             </b>
           </h1>
           {/* School Name */}
-          <h3 className="mt-2 text-center font-inter text-lg text-white sm:text-2xl md:text-3xl xl:mt-8 xl:text-5xl">
+          <h3 className="mt-2 text-center font-inter text-lg text-white sm:text-2xl md:text-3xl xl:text-3xl">
             @ UC San Diego
           </h3>
           {/* Email Button */}
           <a
             href="mailto:vgdc@ucsd.edu"
             target="_blank"
-            className="text-md mx-auto mt-3 block h-7 w-36 rounded-3xl bg-hot-pink text-center align-middle 
-							text-white transition-colors hover:bg-pink-700 hover:text-light-grey sm:mt-5 sm:h-9 sm:w-40 sm:pt-1 sm:text-lg
-							xl:mt-8 xl:h-12 xl:w-56 xl:pt-2 xl:text-2xl"
+            className="text-md mx-auto mt-3 block h-fit w-fit rounded-3xl bg-hot-pink 
+							px-3 py-1 text-center align-middle text-white transition-colors hover:bg-pink-700 hover:text-light-grey 
+							sm:mt-5"
           >
             vgdc@ucsd.edu
           </a>
