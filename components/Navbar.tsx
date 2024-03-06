e79450e514d7f5f31642096f718c9769daaa2e7a
Chase Peterson <cep.piano@gmail.com>
2024-03-05 22:22:13 -0800

    Fixed navbar appearing under content

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
   return (
     <>
       {/* Base navbar div, sticky with a blurred background */}
-      <div className="sticky left-0 top-0 h-16 w-full bg-background-black/30 font-light text-text-grey backdrop-blur-lg lg:h-20">
+      <div className="sticky left-0 top-0 z-50 h-16 w-full bg-background-black/30 font-light text-text-grey backdrop-blur-lg lg:h-20">
         {/* Contains all components inside navbar */}
         <div className="w-full px-4 py-2 lg:px-6">
           {/* The logo, changes size and position dynamically */}
