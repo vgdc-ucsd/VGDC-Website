c474931416f268d12b8b3486b1f8fd390f87064f
Chase Peterson <cep.piano@gmail.com>
2024-03-05 17:59:41 -0800

    Updated Tailwind for social links, removed space in mailto link

--- app/page.tsx
+++ app/page.tsx
@@ -3,99 +3,85 @@ import EventList from "@/components/EventList";
 import Contributors from "@/components/Contributors";
 import Footer from "@/components/Footer";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { FaInstagram,FaDiscord, FaFacebook  } from "react-icons/fa";
+import { FaInstagram, FaDiscord, FaFacebook } from "react-icons/fa";
 import { SiMinutemailer } from "react-icons/si";
 
-
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-background-black">
-      <Navbar />
-      <Hero />
-      <Companies />
-      <About />
-      <Engagement />
-      <Niches />
-      <Games />
-      <Footer />
-    </main>
-  );
+	return (
+		<main className="min-h-screen bg-background-black">
+			<Navbar />
+			<Hero />
+			<Companies />
+			<About />
+			<Engagement />
+			<Niches />
+			<Games />
+			<Footer />
+		</main>
+	);
 }
 
 function Hero() {
-  return (
-    <>
-    </>
-  )
+	return <></>;
 }
 
 function Companies() {
-  return (
-    <>
-    </>
-  )
+	return <></>;
 }
 
 function About() {
-  return (
-    <section className = "flex items-center flex-col w-auto mx-4 py-12">
+	const socialLinkStyle =
+		"text-white transition ease-in duration-150 hover:cursor-pointer hover:text-hot-pink";
 
-      <h2 className = " text-white font-bold text-md md:text-xl lg:text-3xl">
-        What is VGDC?
-      </h2>
+	return (
+		<section className="flex items-center flex-col w-auto mx-4 py-12">
+			<h2 className=" text-white font-bold text-md md:text-xl lg:text-3xl">
+				What is VGDC?
+			</h2>
 
-      <div className = "flex items-center flex-col mt-4">
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>TR</AvatarFallback>
-        </Avatar>
+			<div className="flex items-center flex-col mt-4">
+				<Avatar>
+					<AvatarImage src="https://github.com/shadcn.png" />
+					<AvatarFallback>TR</AvatarFallback>
+				</Avatar>
 
-        <div className = "text-center">
-          <p className = "text-white font-bold text-xs mt-2">President</p>
-          <p className = "text-white text-xs">Tyler Roache</p>
-          <p className = "text-text-grey text-xs md:text-sm max-w-md my-0 mt-3">
-            VGDC is a student-run organization at UCSD dedicated to teaching and applying software and artistic skills widely used in the video game industry.
-          </p>
-        </div>
+				<div className="text-center">
+					<p className="text-white font-bold text-xs mt-2">President</p>
+					<p className="text-white text-xs">Tyler Roache</p>
+					<p className="text-text-grey text-xs md:text-sm max-w-md my-0 mt-3">
+						VGDC is a student-run organization at UCSD dedicated to teaching and
+						applying software and artistic skills widely used in the video game
+						industry.
+					</p>
+				</div>
 
-        <span className = "flex flex-row justify-between w-56 mt-4 align-center">
-          <a href = "https://www.instagram.com/vgdc.ucsd/" target="_blank">
-            <FaInstagram className="text-white transition ease-in transition-duration: 150ms hover:cursor-pointer hover:text-hot-pink" size={32}/>
-          </a>
-          <a href = "https://bit.ly/VGDCUCSD" target="_blank">
-            <FaDiscord className="text-white transition ease-in transition-duration: 150ms hover:cursor-pointer hover:text-hot-pink" size={32}/>
-          </a>
-          <a href = "https://www.facebook.com/groups/VGDC.UCSD/" target="_blank">
-            <FaFacebook className="text-white transition ease-in transition-duration: 150ms hover:cursor-pointer hover:text-hot-pink" size={28}/>
-          </a>
-          <a href = "mailto: vgdc@ucsd.edu" target="_blank">
-            <SiMinutemailer className="text-white transition ease-in transition-duration: 150ms hover:cursor-pointer hover:text-hot-pink" size={28}/>
-          </a>
-        </span>
-      </div>
-    </section>
-  )
+				<span className="flex flex-row justify-between w-56 mt-4 align-center">
+					<a href="https://www.instagram.com/vgdc.ucsd/" target="_blank">
+						<FaInstagram className={socialLinkStyle} size={32} />
+					</a>
+					<a href="https://bit.ly/VGDCUCSD" target="_blank">
+						<FaDiscord className={socialLinkStyle} size={32} />
+					</a>
+					<a href="https://www.facebook.com/groups/VGDC.UCSD/" target="_blank">
+						<FaFacebook className={socialLinkStyle} size={28} />
+					</a>
+					<a href="mailto:vgdc@ucsd.edu" target="_blank">
+						<SiMinutemailer className={socialLinkStyle} size={28} />
+					</a>
+				</span>
+			</div>
+		</section>
+	);
 }
 
-
-
 function Engagement() {
-  return (
-    <>
-    </>
-  )
+	return <></>;
 }
 
 function Niches() {
-  return (
-    <>
-    </>
-  )
+	return <></>;
 }
 
 function Games() {
-  return (
-    <>
-    </>
-  )
+	return <></>;
 }
