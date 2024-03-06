9570d08c61f2c8d7399b2b9d2b75d0314078b753
Chase Peterson <cep.piano@gmail.com>
2024-03-05 19:47:43 -0800

    Added Navbar to Events, News, and Officers pages

--- app/officers/page.tsx
+++ app/officers/page.tsx
@@ -1,3 +1,9 @@
+import Navbar from '@/components/Navbar'
+
 export default function Officers() {
-	return <main className="min-h-screen bg-background-black"></main>;
+  return (
+    <main className="min-h-screen bg-background-black">
+      <Navbar />
+    </main>
+  )
 }
