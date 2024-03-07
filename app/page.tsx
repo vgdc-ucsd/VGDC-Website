c954d70c772545bb5f417076577b719dea632b71
Chase Peterson <cep.piano@gmail.com>
2024-03-06 18:38:44 -0800

    Made full-page view bigger

--- app/page.tsx
+++ app/page.tsx
@@ -29,13 +29,13 @@ function Hero() {
       {/* The full container */}
       <div className="relative h-72 w-full sm:h-96 md:h-[26rem] xl:h-[32rem]">
         <div className="absolute left-0 top-0 w-full">
-          <div className="relative mx-auto w-80 sm:w-[28rem] md:w-[32rem] xl:w-[38rem]">
+          <div className="relative mx-auto w-80 sm:w-[28rem] md:w-[32rem] xl:w-[64rem]">
             <Image
               src="/icons/controller.svg"
               alt="Controller icon"
               width={128}
               height={128}
-              className="absolute left-4 top-16 w-12 sm:w-20 md:top-24 xl:top-36 xl:w-24"
+              className="absolute left-4 top-16 w-12 sm:w-20 md:top-16 xl:w-36"
             />
 
             <Image
@@ -43,7 +43,7 @@ function Hero() {
               alt="Controller icon"
               width={128}
               height={128}
-              className="absolute right-4 top-16 w-12 sm:w-20 md:top-24 xl:top-36 xl:w-24"
+              className="absolute right-4 top-16 w-12 sm:w-20 md:top-16 xl:w-36"
             />
           </div>
         </div>
@@ -54,7 +54,7 @@ function Hero() {
             className="mt-24 inline-block
 							bg-gradient-to-r from-vgdc-light-blue to-vgdc-light-green bg-clip-text 
 							text-center font-inter text-3xl font-extrabold text-transparent
-							sm:mt-32 sm:text-4xl md:mt-40 xl:mt-56 xl:text-5xl"
+							sm:mt-32 sm:text-4xl xl:text-8xl"
           >
             <b>
               Video Game
@@ -63,7 +63,7 @@ function Hero() {
             </b>
           </h1>
           {/* School Name */}
-          <h3 className="mt-2 text-center font-inter text-lg text-white sm:text-2xl md:text-3xl xl:text-3xl">
+          <h3 className="mt-4 text-center font-inter text-lg text-white sm:text-2xl md:text-3xl xl:text-4xl">
             @ UC San Diego
           </h3>
           {/* Email Button */}
