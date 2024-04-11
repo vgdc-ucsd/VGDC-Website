05becf3114fe537cb0a519d0f5234f5122f9e4c4
William Kim <wik002@sdsc.edu>
2024-04-11 10:17:42 -0700

    centered text and added home button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import Image from 'next/image'
+import Link from 'next/link';
 
 
 export function ButtonDemo() {
@@ -9,29 +10,32 @@ export function ButtonDemo() {
 export default function Custom404() {
   
     return (
-      <main className="min-h-screen bg-background-black">
-          <div className="flex flex-col text-center">
-
+      <main className="min-h-screen bg-background-black h-lvh">
+            <div className = "h-full flex flex-col justify-center items-center" >
             <Image
               src="/icons/laptop.svg"
               alt="Controller icon"
               width={140}
               height={140}
-              className="top-16 w-12 sm:w-20 xl:w-24 sm:top-28 md:top-28"
+              className="top-16 w-12 sm:w-20 xl:w-24 sm:top-28 md:top-28 mx-auto"
             />
             <h1
-              className="mt-24 inline-block
+              className="inline-block
                 bg-gradient-to-r from-vgdc-light-blue to-vgdc-light-green bg-clip-text 
                 text-center font-inter text-3xl font-extrabold text-transparent
-                sm:mt-40 sm:text-5xl md:text-5xl xl:text-7xl"
+                sm:text-5xl md:text-5xl xl:text-7xl"
             >404</h1>
 
             <h3 className="mt-2 text-center font-inter text-base xl:text-lg text-white">
-            Uh oh! We couldnt find the page you were looking for!
+              Uh oh! We couldnt find the page you were looking for!
             </h3>
 
-            <Button>Home</Button>
+            <Link href = "/" className = "mx-auto mt-12">
+              <Button>Home</Button>
+            </Link>
+
           </div>
+
       </main>
     );
   }
