dc51bee2a4f5e5923def47c325af32966b54efa3
Chase Peterson <cep.piano@gmail.com>
2024-04-15 15:50:00 -0700

    Fixed post generation, moved everything back to app/page.tsx

--- app/pages/posts/[slug].tsx
+++
@@ -1,3 +0,0 @@
-/**
- * Dynamic page to render posts
- */
--- lib/post.tsx
+++ lib/post.tsx
@@ -1,35 +1,34 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
+import fs from "fs"
+import path from "path"
+import matter from "gray-matter"
 
-const postsDirectory = path.join(process.cwd(), '../posts');
+const postsDirectory = path.join(process.cwd(), "/posts")
 
 export type Post = {
-  id: string;
-  date: string;
-  title: string;
-  content: string;
+  id: string
+  date: string
+  title: string
+  content: string
 }
-
 
 export function getSortedPostsData() {
   console.log("getting all posts")
   // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory);
+  const fileNames = fs.readdirSync(postsDirectory)
   const allPostsData: Post[] = fileNames.map((fileName) => {
     // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, '');
+    const id = fileName.replace(/\.md$/, "")
 
     // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const fullPath = path.join(postsDirectory, fileName)
+    const fileContents = fs.readFileSync(fullPath, "utf8")
 
     // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+    const matterResult = matter(fileContents)
 
     // Ensure that the required properties exist
     if (!matterResult.data.date || !matterResult.data.title) {
-      throw new Error(`Missing date or title in file: ${fileName}`);
+      throw new Error(`Missing date or title in file: ${fileName}`)
     }
 
     // Combine the data with the id
@@ -37,20 +36,19 @@ export function getSortedPostsData() {
       id,
       date: matterResult.data.date,
       title: matterResult.data.title,
-      content: matterResult.content
-    };
-  });
+      content: matterResult.content,
+    }
+  })
 
   // Sort posts by date
   const sortedPosts = allPostsData.sort((a, b) => {
     if (a.date < b.date) {
-      return 1;
+      return 1
     } else {
-      return -1;
+      return -1
     }
-  });
+  })
 
   // Return only the two most recent posts
-  return sortedPosts.slice(0, 2);
-  
+  return sortedPosts.slice(0, 2)
 }
