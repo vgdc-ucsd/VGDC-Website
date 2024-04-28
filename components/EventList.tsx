95ddba56eb7e740d8480a767f649a1e82016bccc
Chase Peterson <cep.piano@gmail.com>
2024-04-27 18:05:34 -0700

    Added filter to display events on the homepage

--- components/EventList.tsx
+++ components/EventList.tsx
@@ -27,7 +27,7 @@ export default async function EventList() {
           image: response.data[i][6],
         }
 
-        eventList.push(event)
+        if (response.data[i][7] == "TRUE") eventList.push(event)
       }
     }
 
