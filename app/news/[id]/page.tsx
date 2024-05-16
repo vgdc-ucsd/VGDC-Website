55abdcc0772b8a741889045c90ea5e3e8fbbdab1
William Kim <91165559+will-loves-coding82@users.noreply.github.com>
2024-05-16 17:32:15 +0000

    polishing blog metadata style

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -7,8 +7,6 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { createAvatar } from '@dicebear/core';
 import { notionistsNeutral } from '@dicebear/collection';
-import { useMemo } from "react";
-
 /**
  * This page holds the content for a 
  * specific blog post. It fetches the id of the 
@@ -37,55 +35,57 @@ export default async function Page({ params }: { params: { id: string } }) {
         <main className="min-h-screen bg-background-black">
             <Navbar />
 
-            <div className="mx-auto max-w-[920px] pb-20 text-white mt-20  flex flex-row justify-center">
+            <div className="mx-auto max-w-[920px] pb-20 text-white mt-6 md:mt-20 flex-col flex md:flex-row justify-center">
 
-                <div id="socialShares" className="sticky left-0 top-0 mr-24 hidden lg:block">
+
+                <div id="socialShares" className="sticky left-0 top-0 mr-20 md:block mx-6 mb-4">
                     <Link href="/">
                         <Button>Back</Button>
                     </Link>
                 </div>
 
-                <article className="mx-6">
-                    <h1 className="text-3xl md:text-5xl mb-2 font-extrabold text-white">{post.title}</h1>
-                    <div className="text-text-grey flex flex-col w-full md:flex-row justify-between md:w-2/3 mt-2 mb-8">
-                        <span className = "flex flex-row align-middle">
+
+                <article className=" mx-6 align-middle">
+
+                    {/**Blog MetaData */}
+                    <div className="text-text-grey flex w-full flex-row justify-between md:w-2/3 mb-8">
+
+                        <div className="flex flex-row align-middle">
                             <img
                                 src={avatar}
                                 className="w-8 mr-2"
-                                alt = "author cover image"
+                                alt="author cover image"
                             />
-                            <p className="text-base">{post.author}</p>
-                        </span>
-                        
-                        
-                        <DateFormat dateString={post.date} />
-                        <p className="text-base">{calculateReadingTime(post.content)} Min Read</p>
-                        <hr className="mt-2" ></hr>
+                            <div>
+                                <p className="text-sm">{post.author}</p>
+                                    {/* <p className="text-xs sm:text-sm">{calculateReadingTime(post.content)} Min Read</p>  */}
+                                    <DateFormat dateString={post.date} />
+                            </div>
+
+                        </div>
                     </div>
 
+                    {/**Cover Image */}
                     <Image
                         src={post.coverImage}
                         width={800}
                         height={600}
-                        alt="Picture of the author"
+                        alt="Cover Image"
                         style={{
                             objectFit: "cover",
                             borderRadius: "16px",
                             height: '40vh',
-                            width: '100%'
+                            width: '100%',
                         }}
                     />
 
+                    <h1 className="text-3xl md:text-5xl mt-4 mb-2 font-extrabold text-white">{post.title}</h1>
 
 
-
-                    <ReactMarkdown className="text-text-grey mt-4 max-w-lg text-base">{post.content}</ReactMarkdown>
-                    {/* <div dangerouslySetInnerHTML={{ __html: post.contentHtml }} /> */}
+                    {/**Blog Content */}
+                    <ReactMarkdown className="text-text-grey mt-4 text-sm md:text-base">{post.content}</ReactMarkdown>
 
                 </article>
-
-
-
             </div>
         </main>
 
@@ -94,7 +94,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 
 function DateFormat({ dateString }: { dateString: string }) {
     const date = parseISO(dateString);
-    return (<time className="text-text-grey" dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>);
+    return (<time className="text-text-grey text-xs sm:text-sm" dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>);
 }
 
 function calculateReadingTime(content: string) {
