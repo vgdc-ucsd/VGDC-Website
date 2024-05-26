d9b6ec21c81b6e45cf813d51fcd642feca057f2d
William Kim <wik002@sdsc.edu>
2024-05-26 16:05:31 -0700

    added back button link

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -2,6 +2,7 @@ import BlogCard from '@/components/BlogCard';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar'
 import { Post, getSortedPostsData } from '@/lib/post';
+import Link from 'next/link';
 
 export default async function News() {
   const posts = await getSortedPostsData(0);
@@ -13,14 +14,19 @@ export default async function News() {
       <div className="mx-auto max-w-[920px] pb-20 text-white">
         {/* Title section */}
         <div className="mx-6 mb-20 mt-20">
-          <h2 className="mb-3 mt-2 text-4xl font-bold">VGDC News</h2>
+          <Link
+            href="./"
+            className="text-text-grey transition-all hover:text-white"
+          >{`<- back to home`}</Link>
+
+          <h2 className="mt-2 text-4xl font-bold">VGDC News</h2>
 
         </div>
      
-      <div className="mx-6 flex-start flex flex-wrap justify-center gap-12">
+      <div className="mx-6 flex-start flex flex-wrap justify-center">
         {posts.map((post:Post, index)=> {
           return (
-          <BlogCard post = {post}/>
+          <BlogCard key={index} post = {post}/>
           )
         })}
       </div>
