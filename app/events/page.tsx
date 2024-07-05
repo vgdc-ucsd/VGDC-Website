0ceba164708410d263fd613e2e000602a26eca47
Chase Peterson <cep.piano@gmail.com>
2024-07-05 11:54:00 -0700

    Added comment to events page.

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar"
 import EventList from "@/components/EventList"
 import Footer from "@/components/Footer"
 
+/** Dedicated events page. */
 export default function Events() {
   return (
     <main className="min-h-screen bg-background-black">
