57c9b5f28d09e62fdc4c9acfff79d4259ea7f393
Eatyboy <141588549+Eatyboy@users.noreply.github.com>
2025-12-09 14:55:02 -0800

    Updated store page form link

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -8,22 +8,22 @@ import Link from "next/link"
 export const revalidate = 60
 
 export default async function Store() {
-  const storeData = await getStoreItems()
+    const storeData = await getStoreItems()
 
-  return (
-    <main className="min-h-screen bg-background-black">
-      <Navbar />
-      <PageSection 
-        heading="Club Merch"
-        subText={
-          <>
-            Browse our current merch collection! Click on any item to purchase or <Link href="https://docs.google.com/forms/d/e/1FAIpQLSe6t-887BQ3EJnKMSihdEvyexDBpEX6ngOeY787Z0RHgafK4g/viewform" className="text-hot-pink">here</Link>.
-          </>
-        }
-      >
-        <StoreVendor data={storeData} />
-      </PageSection>
-      <Footer />
-    </main>
-  )
+    return (
+        <main className="min-h-screen bg-background-black">
+            <Navbar />
+            <PageSection
+                heading="Club Merch"
+                subText={
+                    <>
+                        Browse our current merch collection! Click on any item to purchase or <Link href="https://forms.gle/hbeMy1HY5LHC886L6" className="text-hot-pink">here</Link>.
+                    </>
+                }
+            >
+                <StoreVendor data={storeData} />
+            </PageSection>
+            <Footer />
+        </main>
+    )
 }
