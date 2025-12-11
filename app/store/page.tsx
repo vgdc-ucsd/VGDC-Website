ba10a6c994513c333c9e54895e7f20ef41d40821
Eatyboy <141588549+Eatyboy@users.noreply.github.com>
2025-12-11 14:14:45 -0800

    Updated store page form link for individual items

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -6,6 +6,7 @@ import StoreVendor from "./components/StoreVendor"
 import Link from "next/link"
 
 export const revalidate = 60
+const storeFormLink = "https://forms.gle/hbeMy1HY5LHC886L6";
 
 export default async function Store() {
     const storeData = await getStoreItems()
@@ -17,11 +18,11 @@ export default async function Store() {
                 heading="Club Merch"
                 subText={
                     <>
-                        Browse our current merch collection! Click on any item to purchase or <Link href="https://forms.gle/hbeMy1HY5LHC886L6" className="text-hot-pink">here</Link>.
+                        Browse our current merch collection! Click on any item to purchase or <Link href={storeFormLink} className="text-hot-pink">here</Link>.
                     </>
                 }
             >
-                <StoreVendor data={storeData} />
+                <StoreVendor data={storeData} formLink={storeFormLink} />
             </PageSection>
             <Footer />
         </main>
