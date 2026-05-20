60beb147fdd9623961738cae0fc8c58e24cc03dc
Nhat Tran <nhatt.dev@gmail.com>
2026-05-19 22:32:48 -0700

    Officer role check for signing in

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,9 +6,33 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID!,
       clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: "identify email guilds.members.read",
+        },
+      },
     }),
   ],
   callbacks: {
+    async signIn({ account }) {
+      try {
+        if (account && account.access_token) {
+          const GUILD_MEMBER_URL = `https://discord.com/api/users/@me/guilds/${process.env.DISCORD_GUILD_ID!}/member`
+          const response = await fetch(GUILD_MEMBER_URL, {
+            headers: {
+              "Authorization": `Bearer ${account.access_token!}`,
+            }
+          })
+          const guild_member_info = await response.json()
+          const roles = guild_member_info.roles as string[]
+          if (roles.includes(process.env.DISCORD_OFFICER_ROLE_ID!)) {
+            return true
+          }
+        }
+      } catch (_) { }
+
+      return false
+    },
     async session({ session, token }) {
       if (session.user) {
         (session.user as any).id = token.sub!
