28d810b7fb03542ff43bebf8e13615c29d2af880
Nhat Tran <nhatt.dev@gmail.com>
2026-05-27 14:24:49 -0700

    Enable overscroll and set html color to footer color

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-footer-grey">
       <body className={inter.className}>
         <main>{children}</main>
         <Toaster />
